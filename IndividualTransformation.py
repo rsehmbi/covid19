ce92d79395398d6924b8ef40311e4feab6c8dda8
rsehmbi <rsehmbi@sfu.ca>
2020-11-17 13:42:52 -0800

    Updates the dataset;2A

--- IndividualTransformation.py
+++ IndividualTransformation.py
@@ -5,26 +5,28 @@ Individual = pd.read_csv("dataset/RevisedIndividualData.csv")
 Individual['age'] = Individual['age'].fillna(0)
 
 Individual['sex'].replace({'female': 0, 'male': 1}, inplace=True)
-Individual['RecoveryRate'] = Individual['outcome'].replace(
-    {'deceased': 0, 'hospitalized': 0, 'nonhospitalized': 0, 'recovered': 1})
-Individual['Hospitalizations'] = Individual['outcome'].replace(
-    {'deceased': 0, 'hospitalized': 1, 'nonhospitalized': 0, 'recovered': 0})
-Individual['NonHospitalizations'] = Individual['outcome'].replace(
-    {'deceased': 0, 'hospitalized': 0, 'nonhospitalized': 1, 'recovered': 0})
-Individual['deceased'] = Individual['outcome'].replace(
-    {'deceased': 1, 'hospitalized': 0, 'nonhospitalized': 0, 'recovered': 0})
+Individual['country'] = Individual['country'].str.replace(
+    'United States', 'US')
+# Individual['RecoveryRate'] = Individual['outcome'].replace(
+#     {'deceased': 0, 'hospitalized': 0, 'nonhospitalized': 0, 'recovered': 1})
+# Individual['Hospitalizations'] = Individual['outcome'].replace(
+#     {'deceased': 0, 'hospitalized': 1, 'nonhospitalized': 0, 'recovered': 0})
+# Individual['NonHospitalizations'] = Individual['outcome'].replace(
+#     {'deceased': 0, 'hospitalized': 0, 'nonhospitalized': 1, 'recovered': 0})
+# Individual['deceased'] = Individual['outcome'].replace(
+#     {'deceased': 1, 'hospitalized': 0, 'nonhospitalized': 0, 'recovered': 0})
 
 # Grouped_Individual_Age = Individual.groupby(['country', 'province']).agg(
 #     {'age': 'mean', 'sex': 'mean', 'RecoveryRate': 'mean', 'Hospitalizations': 'mean', 'NonHospitalizations': 'mean', 'deceased': 'mean'}).reset_index()
 
-Individual['RecoveryRate'] = Individual['RecoveryRate'].mul(
-    25 / 100)
-Individual['Hospitalizations'] = Individual['Hospitalizations'].mul(
-    .25)
-Individual['NonHospitalizations'] = Individual['NonHospitalizations'].mul(
-    .25)
-Individual['deceased'] = Individual['deceased'].mul(
-    25 / 100)
+# Individual['RecoveryRate'] = Individual['RecoveryRate'].mul(
+#     25 / 100)
+# Individual['Hospitalizations'] = Individual['Hospitalizations'].mul(
+#     .25)
+# Individual['NonHospitalizations'] = Individual['NonHospitalizations'].mul(
+#     .25)
+# Individual['deceased'] = Individual['deceased'].mul(
+#     25 / 100)
 
 print(Individual)
 Individual.to_csv("MergeData/IndividualData.csv")
