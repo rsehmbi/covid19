efe08064753cecc902d8c36b393ff56570da6814
rsehmbi <rsehmbi@sfu.ca>
2020-11-17 14:01:46 -0800

    Adds the Traing and Test Data Set

---
+++ TrainTestModel.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import random
+
+
+def train_test_split(df):
+    df = df.reset_index()
+    row, col = df.shape
+    test_size = round(0.2 * row)
+    indices = df.index.tolist()
+    test_indices = random.sample(population=indices, k=test_size)
+
+    test_df = df.loc[test_indices]
+    train_df = df.drop(test_indices)
+
+    return train_df, test_df
+
+
+def main():
+    df = pd.read_csv("MergeData/CombinedData.csv")
+    train, test = train_test_split(df)
+    train.to_csv("TrainTestData/TrainingData.csv")
+    test.to_csv("TrainTestData/TestData.csv")
+
+
+if __name__ == "__main__":
+    main()
